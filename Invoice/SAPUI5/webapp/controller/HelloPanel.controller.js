6217579519d64e46db341a3b559ed5a41d543aa2
cazz7 <cazz182@gmail.com>
2021-04-14 16:17:03 +0000

    Commit for lab12

--- Invoice/SAPUI5/webapp/controller/HelloPanel.controller.js
+++ Invoice/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -3,14 +3,16 @@
 /*para llamar al fragment por código*/
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/base/Log"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller } Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
+     * @param {typeof sap.base.Log} Log 
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Log) {
         "use strict"
 
         return Controller.extend("logaligroup.SAPUI5.controller.HelloPanel", {
@@ -27,6 +29,15 @@ sap.ui.define([
             },
             onOpenDialog: function () {
                 this.getOwnerComponent().openHelloDialog();
+            },
+            onBeforeRendering: function(){
+                window.message = 'Log message - onBeforeRendering';
+                Log.info(window.message);
+                Log.error(window.message);
+            },
+            onAfterRendering: function(){
+                //App will stop with developper tools
+                //debugger;
             }
         });
     });
